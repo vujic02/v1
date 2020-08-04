f36bcf4976505e20a8b4610398e648841cfb5b06
vujic02 <vujicn02@gmail.com>
2020-08-04 17:20:51 +0200

    v1

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const portfolioItems = document.querySelectorAll('.portfolio-item')
 
 portfolioItems.forEach(item => {
-item.addEventListener('click', () => {
-        let selected = item.querySelector('.portfolio-overlay');
-        selected.style.opacity = '1';
+item.addEventListener('mouseover', () => {
+        let selectedPortfolioInfo = item.querySelector('.portfolio-info');
+        let selectedIcon = item.querySelector('.links');
+        let selectedPortfolio = item.querySelector('.portfolio-overlay');
+        selectedPortfolio.style.opacity = '1';
+        selectedIcon.style.opacity = '1';
+        selectedPortfolioInfo.style.opacity = '1';
+    });
+    item.addEventListener('mouseout', () => {
+        let selectedIcon = item.querySelector('.links');
+        let selectedPortfolio = item.querySelector('.portfolio-overlay');
+        selectedPortfolio.style.opacity = '0';
+        selectedPortfolio.style.transition = 'ease-in-out 1s';
+        selectedIcon.style.opacity = '0';
+        selectedPortfolioInfo.style.opacity = '0';
     });
 });
